daacbf91b8e0d5a4e9b85805eadc25d8b7c6068c
Jack Sell <98410625+jhdunc@users.noreply.github.com>
2023-05-21 11:28:01 +0200

    Database & Spawning
    Fixed item database and item spawning

--- Assets/Exports/Inventory/ScriptableObjects/Items/Scripts/ConsumableObject.cs
+++ Assets/Exports/Inventory/ScriptableObjects/Items/Scripts/ConsumableObject.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Consumable Object", menuName = "Inventory System/Items/Consumable")]
 public class ConsumableObject : ItemObject
 {
+    public int restoreAmmoValue;
     public void Awake()
     {
         type = ItemType.Consumable;
